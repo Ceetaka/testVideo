79e95e55df976b97488eaf19c61c71f5b0f1dc93
Takashi Onoyama <cetaka1973@gmail.com>
2016-11-16 17:49:25 +0800

    Initial Commit

---
+++ testVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testVideo
+//
+//  Created by 小野山　隆 on 2016/11/16.
+//  Copyright © 2016年 Takashi Onoyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
