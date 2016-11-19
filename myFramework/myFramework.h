51855ebd4130bd35ccc55a37b1af48898c3bd7c8
Takashi Onoyama <cetaka1973@gmail.com>
2016-11-19 18:25:34 +0800

    video test

---
+++ myFramework/myFramework.h
@@ -0,0 +1,19 @@
+//
+//  myFramework.h
+//  myFramework
+//
+//  Created by 小野山　隆 on 2016/11/16.
+//  Copyright © 2016年 Takashi Onoyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for myFramework.
+FOUNDATION_EXPORT double myFrameworkVersionNumber;
+
+//! Project version string for myFramework.
+FOUNDATION_EXPORT const unsigned char myFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myFramework/PublicHeader.h>
+
+
--- testVideo/ViewController.swift
+++ testVideo/ViewController.swift
@@ -7,19 +7,141 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
+    
+    
+    // ビデオのアウトプット.
+    private var myVideoOutput: AVCaptureMovieFileOutput!
+    
+    // スタートボタン.
+    private var myButtonStart: UIButton!
+    
+    // ストップボタン.
+    private var myButtonStop: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // セッションの作成.
+        let mySession = AVCaptureSession()
+        
+        // デバイス.
+        var myDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        // 出力先を生成.
+        let myImageOutput = AVCaptureStillImageOutput()
+        
+        // デバイス一覧の取得.
+        let devices = AVCaptureDevice.devices()
+        
+        // マイクを取得.
+        let audioCaptureDevice = AVCaptureDevice.devices(withMediaType: AVMediaTypeAudio)
+        
+        // マイクをセッションのInputに追加.
+        let audioInput = try! AVCaptureDeviceInput.init(device: audioCaptureDevice?.first as! AVCaptureDevice)
+        
+        // バックライトをmyDeviceに格納.
+        for device in devices! {
+            if((device as AnyObject).position == AVCaptureDevicePosition.back){
+                myDevice = device as? AVCaptureDevice
+            }
+        }
+        
+        // バックカメラを取得.
+        let videoInput = try! AVCaptureDeviceInput.init(device: myDevice)
+        
+        // ビデオをセッションのInputに追加.
+        mySession.addInput(videoInput)
+        
+        // オーディオをセッションに追加.
+        mySession.addInput(audioInput)
+        
+        // セッションに追加.
+        mySession.addOutput(myImageOutput)
+        
+        // 動画の保存.
+        myVideoOutput = AVCaptureMovieFileOutput()
+        
+        // ビデオ出力をOutputに追加.
+        mySession.addOutput(myVideoOutput)
+        
+        // 画像を表示するレイヤーを生成.
+        let myVideoLayer = AVCaptureVideoPreviewLayer.init(session: mySession)
+        myVideoLayer?.frame = self.view.bounds
+        myVideoLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        
+        // Viewに追加.
+        self.view.layer.addSublayer(myVideoLayer!)
+        
+        // セッション開始.
+        mySession.startRunning()
+        
+        // UIボタンを作成.
+        myButtonStart = UIButton(frame: CGRect(x: 0, y: 0, width: 120, height: 50))
+        myButtonStop = UIButton(frame: CGRect(x: 0, y: 0, width: 120, height: 50))
+        
+        myButtonStart.backgroundColor = UIColor.red
+        myButtonStop.backgroundColor = UIColor.gray
+        
+        myButtonStart.layer.masksToBounds = true
+        myButtonStop.layer.masksToBounds = true
+        
+        myButtonStart.setTitle("撮影", for: .normal)
+        myButtonStop.setTitle("停止", for: .normal)
+        
+        myButtonStart.layer.cornerRadius = 20.0
+        myButtonStop.layer.cornerRadius = 20.0
+        
+        myButtonStart.layer.position = CGPoint(x: self.view.bounds.width/2 - 70, y:self.view.bounds.height-50)
+        myButtonStop.layer.position = CGPoint(x: self.view.bounds.width/2 + 70, y:self.view.bounds.height-50)
+        
+        myButtonStart.addTarget(self, action: #selector(ViewController.onClickMyButton), for: .touchUpInside)
+        myButtonStop.addTarget(self, action: #selector(ViewController.onClickMyButton), for: .touchUpInside)
+        
+        // UIボタンをViewに追加.
+        self.view.addSubview(myButtonStart)
+        self.view.addSubview(myButtonStop)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    /*
+     ボタンイベント.
+     */
+    internal func onClickMyButton(sender: UIButton){
+        
+        // 撮影開始.
+        if( sender == myButtonStart ){
+            let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+            
+            // フォルダ.
+            let documentsDirectory = paths[0]
+            
+            // ファイル名.
+            let filePath = "\(documentsDirectory)/test.mp4"
+            
+            // URL.
+            let fileURL = URL(fileURLWithPath: filePath)
+            
+            // 録画開始.
+            myVideoOutput.startRecording(toOutputFileURL: fileURL, recordingDelegate: self)
+            
+        }
+            // 撮影停止.
+        else if ( sender == myButtonStop ){
+            myVideoOutput.stopRecording()
+        }
     }
+    
+    // MARK: - AVCaptureFileOutputRecordingDelegate
+    
+    /*
+     動画がキャプチャーされた後に呼ばれるメソッド.
+     */
+    func capture(_ captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAt outputFileURL: URL!, fromConnections connections: [Any]!, error: Error!) {
 
-
+    }
+    
+    
+    
 }
-
